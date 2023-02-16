f9bd6ab55a98f69cf6c876d41b20cadb310aeada
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2023-02-16 22:24:00 +0600

    icons library will be installed

--- src/Components/PostShare/PostShare.jsx
+++ src/Components/PostShare/PostShare.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './PostShare.css'
 import profileImage from '../../img/profileImg.jpg'
+import {  } from 'react-icons/fa';
+
 const PostShare = () => {
     return (
+
         <div className='PostShare'>
             <img src={profileImage} alt="" />
+            <div>
+                <input type="text" name="" id="" placeholder="What's on your mind?" />
+            </div>
+            <div className='postOptions'>
+                <div className='options'></div>
+            </div>
         </div>
+
+
     );
 };
 
