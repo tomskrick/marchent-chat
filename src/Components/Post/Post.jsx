6e008de0ba8ef82e663e552048437fbd1cff59a4
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2023-02-16 23:45:02 +0600

    photo show on home page work will be do

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Post.css'
+const Post = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Post;
--- src/Components/PostShare/PostShare.jsx
+++ src/Components/PostShare/PostShare.jsx
@@ -2,33 +2,64 @@ import React from 'react';
 import './PostShare.css'
 import profileImage from '../../img/profileImg.jpg'
 // import {  } from 'react-icons/fa';
-import { PhotoIcon, VideoCameraIcon,MapPinIcon,CalendarDaysIcon } from '@heroicons/react/24/solid'
+import { PhotoIcon, VideoCameraIcon, MapPinIcon, CalendarDaysIcon ,XCircleIcon} from '@heroicons/react/24/solid'
+import { useState, useRef } from 'react';
 
 const PostShare = () => {
-    return (
+    const [image, setImage] = useState(null)
+    const imageRef = useRef()
 
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0];
+            setImage({
+                image: URL.createObjectURL(img)
+            })
+        }
+    }
+
+    return (
         <div className='PostShare'>
             <img src={profileImage} alt="" />
             <div>
                 <input type="text" name="" id="" placeholder="What's on your mind?" />
-            </div>
-            <div className='postOptions'>
-                <div className='options'>
-                    <PhotoIcon className="h-6 w-6"></PhotoIcon>Photo
+                <div className='postOptions'>
+                    <div className='options' style={{ color: "var(--photo)" }}
+                        onClick={() => imageRef.current.click()}
+                    >
+                        <PhotoIcon className="h-6 w-6"></PhotoIcon>Photo
+                    </div>
+                    <div className='options' style={{ color: "var(--video)" }}>
+                        <VideoCameraIcon className="h-6 w-6"></VideoCameraIcon>Video
+                    </div>
+                    <div className='options' style={{ color: "var(--location)" }}>
+                        <MapPinIcon className="h-6 w-6"></MapPinIcon>Location
+                    </div>
+                    <div className='options' style={{ color: "var(--shedule)" }}>
+                        <CalendarDaysIcon className="h-6 w-6"></CalendarDaysIcon>Shedule
+                    </div>
+                    <button className='button ps-button'>
+                        Share
+                    </button>
+                    <div style={{ display: 'none' }}>
+                        <input
+                            type="file"
+                            name="myImage"
+                            ref={imageRef}
+                            id=""
+                            onChange={onImageChange} />
+                    </div>
                 </div>
-                <div className='options'>
-                    <VideoCameraIcon className="h-6 w-6"></VideoCameraIcon>Video
-                </div>
-                <div className='options'>
-                    <MapPinIcon className="h-6 w-6"></MapPinIcon>Location
-                </div>
-                <div className='options'>
-                    <CalendarDaysIcon className="h-6 w-6"></CalendarDaysIcon>Shedule
-                </div>
+
+                {image && (
+                    <div className="previewImage">
+                        <XCircleIcon onClick={()=>setImage(null)} className='h-6 w-6'></XCircleIcon>
+                        <img src={image.image} alt="" />
+                    </div>
+                )}
+                
             </div>
         </div>
-
-
     );
 };
 
