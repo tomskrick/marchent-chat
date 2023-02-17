8d70699bf9d9740484cb819055996b2215457e92
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2023-02-17 10:01:08 +0600

    can be added trending data for local

--- src/Components/TrendCard/TrendCard.jsx
+++ src/Components/TrendCard/TrendCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './TrendaCard.css'
+import { TrendData } from '../../Data/TrendData';
 const TrendCard = () => {
     return (
         <div className='TrendCard'>
-            
+            <h3>Trend For you</h3>
+            {TrendData.map((trend) => {
+                return (
+                    <div className="trend">
+                        <span>#{ trend.name}</span>
+                        <span>{ trend.shares}k Shares</span>
+                    </div>
+                )
+            })}
         </div>
     );
 };
